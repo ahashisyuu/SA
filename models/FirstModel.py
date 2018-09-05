d0b35074ce4471e0cb697f809e53b234af52a3b4
ahashi_syuu <1347324360@qq.com>
2018-09-05 19:43:37 +0800

    final  from ahashi

--- models/FirstModel.py
+++ models/FirstModel.py
@@ -11,11 +11,13 @@ class FirstModel(ExampleModel):
         :return: output， 整个模型预测的概率，形状应为(B, class_num),
             class_num可由self.class_num得到
         """
-        gru_units = 128   # 暂时未将该参数加入默认值内，有需要再加入
+        gru_units = 256   # 暂时未将该参数加入默认值内，有需要再加入
         act_func = 'tanh'
 
         encoding_doc = Bidirectional(GRU(gru_units, activation=act_func,
-                                         return_sequences=False))(doc)   # (B, 2*dim)
+                                         return_sequences=True))(doc)
+        encoding_doc = Bidirectional(GRU(gru_units, activation=act_func,
+                                         return_sequences=False))(encoding_doc)   # (B, 2*dim)
         output = Dense(units=self.class_num, activation='softmax')(encoding_doc)
         return output
 
